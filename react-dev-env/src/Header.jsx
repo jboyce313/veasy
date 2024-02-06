9a611e24b70057086fec9cddedd8d39af4026450
Jacob Boyce <jboyce313@gmail.com>
2024-02-06 14:16:20 -0500

    create header component

---
+++ react-dev-env/src/Header.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <>
+      <h1>Veasy</h1>
+    </>
+  );
+}
---
+++ react-dev-env/src/Homepage.jsx
@@ -0,0 +1,7 @@
+export function Homepage() {
+  return (
+    <>
+      <p>Hello</p>
+    </>
+  );
+}
