911c7ced3da056aa179506341e1c68c74293de4b
Jacob Boyce <jboyce313@gmail.com>
2024-03-06 12:46:11 -0600

    css changes

--- react-app/src/components/Regular.jsx
+++ react-app/src/components/Regular.jsx
@@ -1,14 +1,18 @@
 export function Regular({ recipe }) {
   return (
     <div className="regular">
-      <h5>Ingredients</h5>
-      {recipe.regular.ingredients.map((ingredient) => (
-        <p key={ingredient}>{ingredient}</p>
-      ))}
-      <h5>Instructions</h5>
-      {recipe.regular.instructions.map((instruction) => (
-        <p key={instruction}>{instruction}</p>
-      ))}
+      <div className="ingredients">
+        <h5>Ingredients</h5>
+        {recipe.regular.ingredients.map((ingredient) => (
+          <p key={ingredient}>{ingredient}</p>
+        ))}
+      </div>
+      <div className="instructions">
+        <h5>Instructions</h5>
+        {recipe.regular.instructions.map((instruction) => (
+          <p key={instruction}>{instruction}</p>
+        ))}
+      </div>
     </div>
   );
 }
--- react-app/src/components/Vegan.jsx
+++ react-app/src/components/Vegan.jsx
@@ -1,14 +1,18 @@
 export function Vegan({ recipe }) {
   return (
     <div className="vegan">
-      <h5>Ingredients</h5>
-      {recipe.vegan.ingredients.map((ingredient) => (
-        <p key={ingredient}>{ingredient}</p>
-      ))}
-      <h5>Instructions</h5>
-      {recipe.vegan.instructions.map((instruction) => (
-        <p key={instruction}>{instruction}</p>
-      ))}
+      <div className="ingredients">
+        <h5>Ingredients</h5>
+        {recipe.vegan.ingredients.map((ingredient) => (
+          <p key={ingredient}>{ingredient}</p>
+        ))}
+      </div>
+      <div className="instructions">
+        <h5>Instructions</h5>
+        {recipe.vegan.instructions.map((instruction) => (
+          <p key={instruction}>{instruction}</p>
+        ))}
+      </div>
     </div>
   );
 }
