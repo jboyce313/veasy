ab948c4b238b92432038e845eb9f104d61c5a66f
Jacob Boyce <jboyce313@gmail.com>
2024-03-11 13:48:14 -0500

    added back and next buttons

--- react-app/src/components/Recipe.jsx
+++ react-app/src/components/Recipe.jsx
@@ -1,18 +1,43 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useState } from "react";
 import "../styles/Recipe.css";
 import { Regular } from "./Regular";
 import { Vegan } from "./Vegan";
 import { CiTimer } from "react-icons/ci";
+import { FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
 
 export function Recipe({ recipes }) {
   const { id } = useParams();
   const recipe = recipes[id - 1];
+  console.log(recipes.length);
 
   const [version, setVersion] = useState("regular");
 
   return (
     <div className="recipeContainer">
+      <div className="arrows">
+        <div>
+          {id == 1 ? (
+            ""
+          ) : (
+            <div className="arrow">
+              <FaArrowLeft />
+              <Link to={`/veasy/recipes/${parseInt(id) - 1}`}>Back</Link>
+            </div>
+          )}
+        </div>
+        <div>
+          {recipes.length == id ? (
+            ""
+          ) : (
+            <div className="arrow">
+              <Link to={`/veasy/recipes/${parseInt(id) + 1}`}>Next</Link>
+              <FaArrowRight />
+            </div>
+          )}
+        </div>
+      </div>
       <div className="recipePage">
         <div className="info">
           <div>
